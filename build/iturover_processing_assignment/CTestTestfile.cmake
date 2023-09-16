fef1f8949fc3e076689b804abe7f028b3d616ccb
enesisaogluu <enesisaogluu@gmail.com>
2023-09-16 20:31:59 +0300

    initial commit

---
+++ build/iturover_processing_assignment/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/enesisaoglu/rover_ws/src/iturover_processing_assignment
+# Build directory: /home/enesisaoglu/rover_ws/build/iturover_processing_assignment
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
